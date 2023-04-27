9fce1e32d4a2885c0f72388a8b80a5f33eb45136
Faith Meades <122907573+FaithsCoding@users.noreply.github.com>
2023-04-27 10:20:46 +0100

    added a category, create, update and destroy for the POST, PUT & DELETE METHODS with comments.

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -61,14 +61,73 @@ router.get("/:id", (req, res) => {
 
 router.post("/", (req, res) => {
   // create a new category
+  Category.create({
+    category_name: req.body.category_name,
+  })
+    .then((dbProductData) => res.json(dbProductData))
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
 
 router.put("/:id", (req, res) => {
   // update a category by its `id` value
+  Category.update(
+    {
+      category_name: req.body.category_name,
+    },
+    {
+      where: {
+        id: req.params.id,
+      },
+    }
+  )
+    //when the db is successful it sends a JSON response with the data to the client using res.json.
+    //We use an if statement to allow for a 404 error and return a repsonse to the user
+    //We use catch to catch any errors and console log them
+    //We have used a HTTP 500, to send a JSON response to the client showing if a server error occured
+    .then((dbCategoryData) => {
+      if (!dbCategoryData) {
+        res.status(404).json({
+          message:
+            "We have not found a category for this id, please try again.",
+        });
+        return;
+      }
+      res.json(dbCategoryData);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
 
 router.delete("/:id", (req, res) => {
   // delete a category by its `id` value
+  Category.destroy({
+    where: {
+      id: req.params.id,
+    },
+  })
+    //when the db is successful it sends a JSON response with the data to the client using res.json.
+    //We use an if statement to allow for a 404 error and return a repsonse to the user
+    //We use catch to catch any errors and console log them
+    //We have used a HTTP 500, to send a JSON response to the client showing if a server error occured
+    .then((dbCategoryData) => {
+      if (!dbCategoryData) {
+        res.status(404).json({
+          message:
+            "We have not found a category for this id, please try again.",
+        });
+        return;
+      }
+      res.json(dbCategoryData);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
 
 module.exports = router;
