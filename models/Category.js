a1f6aef7daf32b65c1d0a4f436dea11dbdcacac6
Faith Meades <122907573+FaithsCoding@users.noreply.github.com>
2023-05-03 10:50:59 +0100

    created the columns in category and products and added comments

--- models/Category.js
+++ models/Category.js
@@ -1,20 +1,33 @@
-const { Model, DataTypes } = require('sequelize');
+//imports the model and datatypes modules from sequelize
+const { Model, DataTypes } = require("sequelize");
 
-const sequelize = require('../config/connection.js');
+//this imports the connection instance from the cofig/connection.js file
+const sequelize = require("../config/connection.js");
 
+//defines the category model by extending the model class
 class Category extends Model {}
 
 Category.init(
   {
-    // define columns
+    // defines the columns and category models schema by using init()
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    category_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
   },
   {
     sequelize,
     timestamps: false,
     freezeTableName: true,
     underscored: true,
-    modelName: 'category',
+    modelName: "category",
   }
 );
-
+//exports the category model so it can be used globally in other files
 module.exports = Category;
--- models/Product.js
+++ models/Product.js
@@ -1,7 +1,7 @@
 // import important parts of sequelize library
-const { Model, DataTypes } = require('sequelize');
+const { Model, DataTypes } = require("sequelize");
 // import our database connection from config.js
-const sequelize = require('../config/connection');
+const sequelize = require("../config/connection");
 
 // Initialize Product model (table) by extending off Sequelize's Model class
 class Product extends Model {}
@@ -9,14 +9,44 @@ class Product extends Model {}
 // set up fields and rules for Product model
 Product.init(
   {
-    // define columns
+    // defines columns of the product table
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrementIdentity: true,
+    },
+    product_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    price: {
+      type: DataTypes.DECIMAL,
+      allowNull: false,
+    },
+    stock: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      defaultValue: 10,
+      validate: {
+        isNumeric: true,
+      },
+    },
+    category_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: "category",
+        key: "id",
+      },
+    },
   },
   {
-    sequelize,
-    timestamps: false,
-    freezeTableName: true,
-    underscored: true,
-    modelName: 'product',
+    //defines the options for the product model
+    sequelize, //this is for the connection
+    timestamps: false, // disables created and updated fields
+    freezeTableName: true, //stops sequalise changin the table name
+    underscored: true, //uses a certain case for the different column names, normally snake_cake in this instance
+    modelName: "product", //specifies the model name
   }
 );
 
